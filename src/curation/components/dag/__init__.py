4468820935444c839e83b06d21a7fb093a3270df
Jonas Ribeiro da Rosa <krnsjrr@gmail.com>
2026-09-08 10:26:10 -0300

    Remove obsolete streamlit-flow dag component

--- src/curation/components/dag/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Fachada para o componente de DAG vivo do pipeline."""
-
-from __future__ import annotations
-
-from .live_flow import build_live_dag_state, ORDERED_STAGES
-
-__all__ = ["build_live_dag_state", "ORDERED_STAGES"]
